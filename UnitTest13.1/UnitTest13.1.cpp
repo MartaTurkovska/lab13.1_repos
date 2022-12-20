32eb0c0dbbe9347cc000d327e6bda5437bc9994a
Marta <turkovska05@gmail.com>
2022-12-20 20:38:41 +0200

    sex,drugs,etc

---
+++ UnitTest13.1/UnitTest13.1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_13.1/var.h"
+#include "../lab_13.1/var.cpp"
+#include "../lab_13.1/dod.h"
+#include "../lab_13.1/dod.cpp"
+#include "../lab_13.1/sum.h"
+#include "../lab_13.1/sum.cpp"
+#include "../lab_13.1/lab_13_1.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest131
+{
+	TEST_CLASS(UnitTest131)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			a = 2;
+			x = 4;
+			n = 2;
+			dod();
+			Assert::AreEqual(a, 1.);
+		}
+	};
+}
---
+++ lab_13.1/sum.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////
+// sum.cpp
+// файл реалізації функції
+#include <math.h>
+#include "dod.h"
+#include "sum.h"
+#include "var.h" 
+
+using namespace nsDod;
+using namespace nsVar;
+
+void nsSum::sum() 
+{
+	n = 0;
+	a = (x - 1) / (1. * x);
+	s = a;
+	do {
+		n++;
+		dod(); 
+		s += a;
+	} while (fabs(a) > e);
+}
